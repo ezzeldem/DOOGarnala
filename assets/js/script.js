d7da05a341f44d66ccdf81cd52f0ff7da37e75e5
ezzeldem <ezzelden06@gmail.com>
2022-01-08 15:28:30 +0200

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+
+
+  $('.header_section .opne_menu i').click(function (e) {
+    e.preventDefault();
+    $('.nav_box').addClass('opne_menu');
+    $('.overlay').show();
+  });
+  $('.header_section .nav_box .close_box i, .overlay').click(function (e) {
+    e.preventDefault();
+    $('.nav_box').removeClass("opne_menu");
+    $('.overlay').hide();
+  });
+
+
+
+  $('.clients_slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    outHight: true,
+    autoplaySpeed: 2000,
+    autoplay: false,
+    dots: false,
+    arrows: true,
+    infinite: false,
+    prevArrow: "<div class='circle-arrow slider-left'><i class='fas fa-long-arrow-alt-left'></i></div>",
+    nextArrow: "<div class='circle-arrow slider-right'><i class='fas fa-long-arrow-alt-right'></i></div>",
+
+  });
+
+});
